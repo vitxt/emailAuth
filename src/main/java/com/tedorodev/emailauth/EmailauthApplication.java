efa19a0491a23e1ab52074059bd1e8e396e2bd18
vitxt <teodorovitor30@gmail.com>
2026-03-05 15:30:38 -0300

    first

---
+++ src/main/java/com/tedorodev/emailauth/EmailauthApplication.java
@@ -0,0 +1,13 @@
+package com.tedorodev.emailauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tedorodev/emailauth/EmailauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tedorodev.emailauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
